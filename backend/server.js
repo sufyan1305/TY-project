c5c51dec7d88ced973c4749c65d714b6905c7531
Sufyan Shaikh <sufyans1353@gmail.com>
2024-01-28 17:31:07 +0530

    after changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mysql = require('mysql')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'theka_coffee'
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-})
+
+app.use(cors());
+app.use(express.json());
+connection.connect()
+
+
+
+
+// app.get('/', (req, res) => {
+  //   res.send('Hello World!')
+  // })
+  
+  app.get('/',(req,res)=>{
+    res.json({message:"Connected...."})
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
+  connection.end()
